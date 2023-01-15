ce66446881f3997a403cff33a46a23b4614dabf3
MustakimK <103403514+MustakimK@users.noreply.github.com>
2023-01-15 03:34:35 -0500

    basis for portfolio screen done
    need to implement array thing

--- project/src/screens/PortfolioScreen/index.js
+++ project/src/screens/PortfolioScreen/index.js
@@ -1,10 +1,22 @@
 import React from 'react'
+import './PortfolioScreen.css';
 
 const PortfolioScreen = () => {
 
     return (
-        <div>
-            My Portfolio Screen
+        <div className="App">
+          <h1 className="welcome-text">Welcome {localStorage.getItem('name')}</h1>
+          <div className="balance-container">
+            <p className="balance-text" style={{ fontSize: '20px' }}>Your balance:</p>
+            <p className="balance-amount" style={{ fontSize: '30px', color: 'green' }}>$54,613.54</p>
+          </div>
+          <div className="investment-text">
+            <p>Your Investments:</p>
+          </div>
+          <div className="rounded-rectangle">
+            <p className="left-text">Company 1</p>
+            <p className="right-text">You invested</p>
+          </div>
         </div>
     )
 }
