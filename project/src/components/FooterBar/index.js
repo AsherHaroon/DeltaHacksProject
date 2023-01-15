defd79a5030525da0c008ad5e4400c0b805bb5aa
Hammad Rehman <hammad_rehman77@hotmail.com>
2023-01-15 09:20:53 -0500

    Dashboard stuff

---
+++ project/src/components/FooterBar/index.js
@@ -0,0 +1,14 @@
+import React, { useState } from "react";
+import './FooterBar.css';
+
+const FooterBar = () => {
+    return (
+        <div className="footer-container">
+            <div className="footer-text-container">
+                @ 2023 All Rights Reserved
+            </div>
+        </div>
+    )
+}
+
+export default FooterBar;
---
+++ project/src/components/InvestingGuide/InvestingGuide.js
@@ -0,0 +1,32 @@
+import React from 'react';
+
+<title>Rinvest</title>
+document.body.style = 'background: #F1F1F!'
+
+const InvestingGuide = () => {
+  return (
+    <div>
+      <h1 style={{ color: "#0050a5ff", fontSize: "60px", marginLeft: "24px"}}>How to Invest</h1>
+      <div style={{ display: "flex", alignItems: "center" }}>
+        <div style={{ color: "#ffdf01ff", fontSize: "20px", marginLeft: "24px"}}>
+          <p>Sign Up</p>
+          <p style={{ color: "grey" }}>Create a secure account on Rinvest</p>
+        </div>
+        <div style={{ color: "#ffdf01ff", fontSize: "20px", marginLeft: "36px"}}>
+          <p>Browse Investments</p>
+          <p style={{ color: "grey" }}>Examine a wide variety of investment options, including startups and collectibles, by reviewing hundreds of opportunities</p>
+        </div>
+        <div style={{ color: "#ffdf01ff", fontSize: "20px", marginLeft: "36px"}}>
+          <p>Make an Investment</p>
+          <p style={{ color: "grey" }}>Acquire a financial interest in a startup or collectible by submitting payment</p>
+        </div>
+        <div style={{ color: "#ffdf01ff", fontSize: "20px", marginLeft: "36px", marginRight: "24px"  }}>
+          <p>Hold or Sell</p>
+          <p style={{ color: "grey" }}>On Rinvest's trading platform, you can choose to participate in future funding rounds, hold onto your current investment, or sell eligible securities.</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default InvestingGuide;
